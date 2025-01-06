cc5d8e61fbd55614be5e7fe1e6e2e30d575ba04b
ayushbhatt1412 <ayushbhatt23081@gmail.com>
2025-01-06 05:34:29 +0530

    Update main.py
    done

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-"""
-Bunch of functions
-"""
-
-def add(number_a:int, number_b:int):
-    """
-    This function adds 2 numbers
-    """
-    return number_a  + number_b + 1
+def add(number_a: int, number_b: int):
+ 
     
+    
+    return number_a + number_b
